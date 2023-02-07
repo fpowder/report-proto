e80510782aa2369fdb180192144ceb62d22fc48c
joohyun jo <jjh@alt-a.net>
2023-02-07 20:59:04 +0900

    feature: merge cell, dbconnection pool added

--- router/sheet.js
+++ router/sheet.js
@@ -345,14 +345,32 @@ sheetRouter.get('/set/frame', async(req, res) => {
 
     const startRowIndex = 13, endRowIndex = 16;
     const startColumnIndex = 1, endColumnIndex = 2;
-    
     const rowOffset = 3;
+
+    const startGraphRowIndex = 68, endGraphRowIndex = 71;
+    const graphRowOffset = 15;
+
     const sheetId = 0;
 
     const insCnt = 13;
 
     const requests = [];
 
+    // '합계' 입력할 cell merge set
+    requests.push({
+        mergeCells: {
+            range: {
+                sheetId,
+                startRowIndex: 10,
+                endRowIndex: 13,
+                startColumnIndex,
+                endColumnIndex
+            },
+            mergeType: 'MERGE_ALL'
+        }
+    });
+
+    // 개소별 현황 테이블을 위한 cell merge
     for(let i = 0; i < insCnt; i++) {
         requests.push({
             mergeCells: {
@@ -360,6 +378,36 @@ sheetRouter.get('/set/frame', async(req, res) => {
                     sheetId,
                     startRowIndex: startRowIndex + (i * rowOffset),
                     endRowIndex: endRowIndex + (i * rowOffset),
+                    startColumnIndex,
+                    endColumnIndex
+                },
+                mergeType: 'MERGE_ALL'
+            }
+        });
+    }
+
+    // 합계 통계 그래프용 cell merge
+    requests.push({
+        mergeCells: {
+            range: {
+                sheetId,
+                startRowIndex: 53,
+                endRowIndex: 56,
+                startColumnIndex,
+                endColumnIndex
+            },
+            mergeType: 'MERGE_ALL'
+        }
+    });
+
+    // 개소별 통계 그래프용 cell merge
+    for(let i = 0; i < insCnt; i++) {
+        requests.push({
+            mergeCells: {
+                range: {
+                    sheetId,
+                    startRowIndex: startGraphRowIndex + (i * graphRowOffset),
+                    endRowIndex: endGraphRowIndex + (i * graphRowOffset),
                     startColumnIndex,
                     endColumnIndex
                 },
